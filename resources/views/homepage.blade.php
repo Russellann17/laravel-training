0cd2dac2f12f7c6b6893dd2ef267ee5ea7182430
Russ <dacalrussell@gmail.com>
2023-01-14 15:30:41 +0800

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.welcome')
+@section('body')
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Id:</label>
+    <input type="email" class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter Id">
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Assignee:</label>
+    <input type="email" class="form-control" id="assignee" aria-describedby="emailHelp" placeholder="Assignee">
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Description</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Status:</label>
+    <select class="form-control">
+  <option>Pending</option>
+  <option>Completed</option>
+  </select>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Created At:</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Created At">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Updated At:</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Updated At">
+  </div>
+  <button type="submit" class="btn btn-primary">Create</button>
+</form>
+@endsection
