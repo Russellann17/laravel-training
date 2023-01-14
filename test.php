852278160874ed7232044c5f0bfb238818ea6271
Russ <dacalrussell@gmail.com>
2023-01-14 15:47:53 +0800

    testraGani

--- test.php
+++ test.php
@@ -1,1 +1,7 @@
-
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Id:</label>
+    <input type="email" class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter Id">
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+</form>
